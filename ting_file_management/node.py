2f69d3656254328dba1002745185a8c783cfecd0
Levy Araujo <levy4raujo@gmail.com>
2023-07-18 17:20:55 -0300

    [Req 1 Concluído]
    Criando uma fila usando Array.

---
+++ ting_file_management/node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def get_value(self):
+        return self.value
+
+    def test(self):
+        return 'Im here'
