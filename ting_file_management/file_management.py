596a6a34cf29f503c9f0f8334c8f734183bc735b
Levy Araujo <levy4raujo@gmail.com>
2023-07-19 13:43:01 -0300

    [Req 2 Concluído]
    Lendo arquivos de texto.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.find('.txt') == -1:
+        print('Formato inválido', file=sys.stderr)
+    try:
+        with open(path_file, 'r') as file:
+            return file.read().split('\n')
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
