6e234e7b270f9f166b4085a9364c52f6a380cfb6
Levy Araujo <levy4raujo@gmail.com>
2023-07-20 13:25:58 -0300

    [Req 5 Concluído]
    Implementando Busca.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -36,4 +36,7 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if instance.__len__() < position:
+        print('Posição inválida', file=sys.stderr)
+    else:
+        sys.stdout.write(str(instance.search(position)))
