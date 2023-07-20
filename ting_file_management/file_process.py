e7471cf98dbd9112075995d7519bd011846f3c16
Levy Araujo <levy4raujo@gmail.com>
2023-07-20 11:35:10 -0300

    [Req 3 Concluído]
    Enfileirando Arquivo.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,29 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
+def numLinhas(dataFile):
+    numLine = 0
+    for i in dataFile:
+        if i:
+            numLine += 1
+    return numLine
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    isAvailable = True
+    for element in instance.data:
+        if element['nome_do_arquivo'] == path_file:
+            isAvailable = False
+    if isAvailable:
+        dataFile = txt_importer(path_file)
+        result = {
+            'nome_do_arquivo': path_file,
+            'qtd_linhas': numLinhas(dataFile),
+            'linhas_do_arquivo': dataFile,
+        }
+        instance.enqueue(result)
+        sys.stdout.write(str(result))
 
 
 def remove(instance):
