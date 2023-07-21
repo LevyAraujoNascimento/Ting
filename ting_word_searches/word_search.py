2605da3183fdd6f84e5412baa244a0d25e69d74f
Levy Araujo <levy4raujo@gmail.com>
2023-07-21 13:44:22 -0300

    [Req 7 Concluído]
    Buscando Palavra nos arquivos.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,26 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação Teste"""
+    finalResult = []
+    for data in instance.data:
+        numLine = 0
+        isAvailable = False
+        result = {
+            "palavra": word.lower(),
+            "arquivo": data["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for line in data['linhas_do_arquivo']:
+            numLine += 1
+            if word.lower() in line.lower():
+                isAvailable = True
+                result["ocorrencias"].append({"linha": numLine})
+        if isAvailable:
+            finalResult.append(result)
+    return finalResult
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+
+
+x = 'Levy é top'
+print('top' in x)
