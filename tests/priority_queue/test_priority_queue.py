f01d6ed7f4c6ecebb0eac1c35f674794f6aa942f
Levy Araujo <levy4raujo@gmail.com>
2023-07-21 11:40:20 -0300

    [Req 6 Concluído]
    Testando Priority Queue.

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,26 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+    # enfileirando os elementos
+    for i in range(10):
+        mock = {
+            "qtd_linhas": i
+        }
+        queue.enqueue(mock)
+    assert queue.__len__() == 10
+    # testando prioridade
+    assert queue.is_priority({"qtd_linhas": 1000}) is False
+    # removendo um elemento
+    queue.dequeue()
+    assert queue.__len__() == 9
+    # buscando um elemento
+    assert queue.search(0) == {"qtd_linhas": 1}
+    # index de busca inválido
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        queue.search(1000)
+
+
+# test_basic_priority_queueing()
