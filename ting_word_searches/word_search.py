8b5a03ce0e73b94614965bce322484e5dc410116
Levy Araujo <levy4raujo@gmail.com>
2023-07-21 13:59:21 -0300

    [Req 8 Concluído]
    Complementando a busca.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -19,8 +19,23 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
-
-
-x = 'Levy é top'
-print('top' in x)
+    finalResult = []
+    for data in instance.data:
+        numLine = 0
+        isAvailable = False
+        result = {
+            "palavra": word.lower(),
+            "arquivo": data["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for line in data['linhas_do_arquivo']:
+            numLine += 1
+            if word.lower() in line.lower():
+                isAvailable = True
+                result["ocorrencias"].append({
+                    "linha": numLine,
+                    "conteudo": line
+                })
+        if isAvailable:
+            finalResult.append(result)
+    return finalResult
